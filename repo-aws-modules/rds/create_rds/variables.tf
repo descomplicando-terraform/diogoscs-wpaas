cdb7861a1b27d5e7470dde85af6f4ed13a0c278f
Diogo Soares <diogoscs@gmail.com>
2024-09-25 19:58:38 -0300

    feat: Adicionando novo modulo de rds

--- repo-aws-modules/rds/create_rds/variables.tf
+++ repo-aws-modules/rds/create_rds/variables.tf
@@ -1,6 +1,114 @@
+variable "name" {
+  description = ""
+  type        = string
+  default     = "project-name"
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where to create security group"
+  type        = string
+  default     = ""
+}
+
 variable "identifier" {
   description = "The name of the RDS instance"
   type        = string
+}
+
+variable "port" {
+  description = "The port on which the DB accepts connections"
+  type        = string
+  default     = null
+}
+
+variable "create_db_subnet_group" {
+  description = "Determines whether to create the database subnet group or use existing"
+  type        = bool
+  default     = false
+}
+
+variable "subnet_group_ids" {
+  description = "List of subnet IDs used by database subnet group created"
+  type        = list(string)
+  default     = []
+}
+
+variable "db_subnet_group_name" {
+  description = "The name of the subnet group name (existing or created)"
+  type        = string
+  default     = "${var.name}-subnetgroup"
+}
+
+variable "db_subnet_group_description" {
+  description = "The description of the DB parameter group"
+  type        = string
+  default     = null
+}
+
+variable "environment" {
+  type        = string
+  description = ""
+  default = "prd"
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
+
+variable "create_db_parameter_group" {
+  description = "Determines whether to create the database subnet group or use existing"
+  type        = bool
+  default     = true
+}
+
+variable "db_parameter_group_name" {
+  description = "The name of the DB parameter group"
+  type        = string
+  default     = ""
+}
+
+variable "db_parameter_group_family" {
+  description = "The family of the DB parameter group"
+  type        = string
+  default     = null
+}
+
+variable "db_parameter_group_description" {
+  description = "The description of the DB parameter group"
+  type        = string
+  default     = null
+}
+
+variable "db_parameter_group_parameters" {
+  description = "A list of DB parameter maps to apply"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "create_db_option_group" {
+  description = "Whether to create this resource or not?"
+  type        = bool
+  default     = true
+}
+
+variable "db_option_group_name" {
+  description = "The name of the option group"
+  type        = string
+  default     = ""
+}
+
+variable "option_group_description" {
+  description = "The description of the option group"
+  type        = string
+  default     = null
+}
+
+variable "major_engine_version" {
+  description = "Specifies the major version of the engine that this option group should be associated with"
+  type        = string
+  default     = null
 }
 
 variable "engine" {
@@ -21,44 +129,56 @@ variable "instance_class" {
   default     = null
 }
 
-variable "db_name" {
-  description = "The DB name to create. If omitted, no database is created initially"
+variable "allocated_storage" {
+  description = "The allocated storage in gigabytes"
+  type        = number
+  default     = null
+}
+
+variable "storage_type" {
+  description = "One of 'standard' (magnetic), 'gp2' (general purpose SSD), 'gp3' (new generation of general purpose SSD), or 'io1' (provisioned IOPS SSD). The default is 'io1' if iops is specified, 'gp2' if not. If you specify 'io1' or 'gp3' , you must also include a value for the 'iops' parameter"
   type        = string
   default     = null
 }
 
-variable "instance_class" {
-  description = "The instance type of the RDS instance"
+variable "options" {
+  description = "A list of Options to apply"
+  type        = any
+  default     = []
+}
+
+variable "storage_encrypted" {
+  description = "Specifies whether the DB instance is encrypted"
+  type        = bool
+  default     = true
+}
+
+variable "kms_key_id" {
+  description = "The ARN for the KMS encryption key. If creating an encrypted replica, set this to the destination KMS ARN. If storage_encrypted is set to true and kms_key_id is not specified the default KMS key created in your account will be used. Be sure to use the full ARN, not a key alias."
   type        = string
   default     = null
 }
 
-variable "db_name" {
-  description = "The DB name to create. If omitted, no database is created initially"
+variable "license_model" {
+  description = "License model information for this DB instance. Optional, but required for some DB engines, i.e. Oracle SE1"
   type        = string
   default     = null
 }
 
-variable "port" {
-  description = "The port on which the DB accepts connections"
-  type        = string
-  default     = null
+variable "create_security_group" {
+  description = "Whether to create this resource or not?"
+  type        = bool
+  default     = true
 }
 
-variable "tags" {
-  description = "A mapping of tags to assign to all resources"
-  type        = map(string)
+variable "security_group_rules" {
+  description = "Map of security group rules to add to the cluster security group created"
+  type        = any
   default     = {}
 }
 
-variable "deletion_protection" {
-  description = "The database can't be deleted when this value is set to true"
-  type        = bool
-  default     = false
-}
-
-variable "username" {
-  description = "Username for the master DB user"
+variable "db_name" {
+  description = "The DB name to create. If omitted, no database is created initially"
   type        = string
   default     = null
 }
@@ -69,22 +189,40 @@ variable "iam_database_authentication_enabled" {
   default     = false
 }
 
-variable "create_db_subnet_group" {
-  description = "Whether to create a database subnet group"
-  type        = bool
-  default     = false
-}
-
-variable "db_subnet_group_name" {
-  description = "Name of DB subnet group. DB instance will be created in the VPC associated with the DB subnet group. If unspecified, will be created in the default VPC"
+variable "network_type" {
+  description = "The type of network stack to use"
   type        = string
   default     = null
 }
 
-variable "subnet_ids" {
-  description = "A list of VPC subnet IDs"
+variable "vpc_security_group_ids" {
+  description = "List of VPC security groups to associate"
   type        = list(string)
   default     = []
+}
+
+variable "availability_zone" {
+  description = "The Availability Zone of the RDS instance"
+  type        = string
+  default     = null
+}
+
+variable "multi_az" {
+  description = "Specifies if the RDS instance is multi-AZ"
+  type        = bool
+  default     = false
+}
+
+variable "iops" {
+  description = "The amount of provisioned IOPS. Setting this implies a storage_type of 'io1' or `gp3`. See `notes` for limitations regarding this variable for `gp3`"
+  type        = number
+  default     = null
+}
+
+variable "publicly_accessible" {
+  description = "Bool to control if instance is publicly accessible"
+  type        = bool
+  default     = false
 }
 
 variable "allow_major_version_upgrade" {
@@ -99,70 +237,10 @@ variable "auto_minor_version_upgrade" {
   default     = true
 }
 
-variable "create_db_parameter_group" {
-  description = "Whether to create a database parameter group"
+variable "apply_immediately" {
+  description = "Specifies whether any database modifications are applied immediately, or during the next maintenance window"
   type        = bool
-  default     = true
-}
-
-variable "parameter_group_name" {
-  description = "Name of the DB parameter group to associate or create"
-  type        = string
-  default     = null
-}
-
-variable "family" {
-  description = "The family of the DB parameter group"
-  type        = string
-  default     = null
-}
-
-variable "parameters" {
-  description = "A list of DB parameters (map) to apply"
-  type        = list(map(string))
-  default     = []
-}
-
-variable "create_db_option_group" {
-  description = "Create a database option group"
-  type        = bool
-  default     = true
-}
-
-variable "option_group_name" {
-  description = "Name of the option group"
-  type        = string
-  default     = null
-}
-
-variable "major_engine_version" {
-  description = "Specifies the major version of the engine that this option group should be associated with"
-  type        = string
-  default     = null
-}
-
-variable "options" {
-  description = "A list of Options to apply"
-  type        = any
-  default     = []
-}
-
-variable "vpc_security_group_ids" {
-  description = "List of VPC security groups to associate"
-  type        = list(string)
-  default     = []
-}
-
-variable "backup_retention_period" {
-  description = "The days to retain backups for"
-  type        = number
-  default     = null
-}
-
-variable "backup_window" {
-  description = "The daily time range (in UTC) during which automated backups are created if they are enabled. Example: '09:46-10:16'. Must not overlap with maintenance_window"
-  type        = string
-  default     = null
+  default     = false
 }
 
 variable "maintenance_window" {
@@ -171,8 +249,147 @@ variable "maintenance_window" {
   default     = null
 }
 
-variable "allocated_storage" {
-  description = "The allocated storage in gigabytes"
+variable "storage_throughput" {
+  description = "Storage throughput value for the DB instance. See `notes` for limitations regarding this variable for `gp3`"
   type        = number
+  default     = null
+}
+
+variable "ca_cert_identifier" {
+  description = "Specifies the identifier of the CA certificate for the DB instance"
+  type        = string
+  default     = null
+}
+
+variable "snapshot_identifier" {
+  description = "Specifies whether or not to create this database from a snapshot. This correlates to the snapshot ID you'd find in the RDS console, e.g: rds:production-2015-06-26-06-05"
+  type        = string
+  default     = null
+}
+
+variable "copy_tags_to_snapshot" {
+  description = "On delete, copy all Instance tags to the final snapshot"
+  type        = bool
+  default     = false
+}
+
+variable "performance_insights_enabled" {
+  description = "Specifies whether Performance Insights are enabled"
+  type        = bool
+  default     = false
+}
+
+variable "performance_insights_retention_period" {
+  description = "The amount of time in days to retain Performance Insights data. Valid values are `7`, `731` (2 years) or a multiple of `31`"
+  type        = number
+  default     = 7
+}
+
+variable "performance_insights_kms_key_id" {
+  description = "The ARN for the KMS key to encrypt Performance Insights data"
+  type        = string
+  default     = null
+}
+
+variable "replicate_source_db" {
+  description = "Specifies that this resource is a Replicate database, and to use this value as the source database. This correlates to the identifier of another Amazon RDS Database to replicate"
+  type        = string
+  default     = null
+}
+
+variable "backup_window" {
+  description = "The daily time range (in UTC) during which automated backups are created if they are enabled. Example: '09:46-10:16'. Must not overlap with maintenance_window"
+  type        = string
+  default     = null
+}
+  
+variable "backup_retention_period" {
+  description = "The days to retain backups for"
+  type        = number
+  default     = null
+}
+
+variable "max_allocated_storage" {
+  description = "Specifies the value for Storage Autoscaling"
+  type        = number
+  default     = 0
+}
+
+variable "monitoring_interval" {
+  description = "The interval, in seconds, between points when Enhanced Monitoring metrics are collected for the DB instance. To disable collecting Enhanced Monitoring metrics, specify 0. The default is 0. Valid Values: 0, 1, 5, 10, 15, 30, 60"
+  type        = number
+  default     = 0
+}
+
+variable "delete_automated_backups" {
+  description = "Specifies whether to remove automated backups immediately after the DB instance is deleted"
+  type        = bool
+  default     = true
+}
+
+variable "deletion_protection" {
+  description = "The database can't be deleted when this value is set to true"
+  type        = bool
+  default     = false
+}
+
+variable "restore_to_point_in_time" {
+  description = "Restore to a point in time (MySQL is NOT supported)"
+  type        = map(string)
+  default     = null
+}
+
+variable "enabled_cloudwatch_logs_exports" {
+  description = "List of log types to enable for exporting to CloudWatch logs. If omitted, no logs will be exported. Valid values (depending on engine): alert, audit, error, general, listener, slowquery, trace, postgresql (PostgreSQL), upgrade (PostgreSQL)"
+  type        = list(string)
+  default     = []
+}
+
+
+variable "timezone" {
+  description = "Time zone of the DB instance. timezone is currently only supported by Microsoft SQL Server. The timezone can only be set on creation. See MSSQL User Guide for more information"
+  type        = string
+  default     = null
+}
+
+variable "character_set_name" {
+  description = "The character set name to use for DB encoding in Oracle instances. This can't be changed. See Oracle Character Sets Supported in Amazon RDS and Collations and Character Sets for Microsoft SQL Server for more information. This can only be set on creation"
+  type        = string
+  default     = null
+}
+
+variable "nchar_character_set_name" {
+  description = "The national character set is used in the NCHAR, NVARCHAR2, and NCLOB data types for Oracle instances. This can't be changed."
+  type        = string
+  default     = null
+}
+
+variable "create_monitoring_role" {
+  description = "Create IAM role with a defined name that permits RDS to send enhanced monitoring metrics to CloudWatch Logs"
+  type        = bool
+  default     = false
+}
+
+variable "monitoring_role_name" {
+  description = "Name of the IAM role which will be created when create_monitoring_role is enabled"
+  type        = string
+  default     = "rds-monitoring-role"
+}
+
+variable "monitoring_role_arn" {
+  description = "The ARN for the IAM role that permits RDS to send enhanced monitoring metrics to CloudWatch Logs. Must be specified if monitoring_interval is non-zero"
+  type        = string
+  default     = null
+}
+
+variable "monitoring_role_permissions_boundary" {
+  description = "ARN of the policy that is used to set the permissions boundary for the monitoring IAM role"
+  type        = string
+  default     = null
+}
+
+variable "monitoring_role_description" {
+  description = "Description of the monitoring IAM role"
+  type        = string
   default     = null
 }
